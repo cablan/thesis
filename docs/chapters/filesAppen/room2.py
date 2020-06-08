0c15657a6044096b7c52fd354786dca05d95747a
cablan <cblasco_95@hotmail.es>
2020-06-08 17:25:16 +0200

    Including chapter 5

---
+++ docs/chapters/filesAppen/room2.py
@@ -0,0 +1,42 @@
+import socket, time, sys
+
+# Predefined variables
+waitingTime = 1
+
+# Choose fileLength
+if len(sys.argv) == 2:
+    fileLength = sys.argv[1]
+else:
+    fileLength = 10
+
+# Open the source file in read mode
+fileName = 'room2.txt'
+fileName = '/home/cablan/Desktop/thesisFiles/inputs/' + fileName
+tuplesFile = open(fileName, 'r')
+
+# Create a socket
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+# Define the host and the port and listen into them
+host = 'localhost'
+port = 8887
+s.bind((host, port))
+s.listen(5)
+
+# Accept the requested connection
+clientSocket, addr = s.accept()
+print("Connection accepted from " + repr(addr[1]))
+
+time.sleep(5)
+
+# Send each tuple contained in the file
+for eachTuple in tuplesFile:
+    print('Sending tuple: ' + '(' + eachTuple[0:-1] + ')')
+    clientSocket.send(bytes(eachTuple, 'utf-8'))
+    time.sleep(waitingTime)
+
+# Close the connection
+clientSocket.close()
+
+# Close the file
+tuplesFile.close()
---
+++ docs/chapters/filesAppen/tempRoom1Sin.py
@@ -0,0 +1,74 @@
+import random, math
+
+import matplotlib.pyplot as plt
+
+def sin(upperBoundary, lowerBoundary, freq, initialDelta):
+    # Fix Variables
+    midValue = lowerBoundary + (upperBoundary - lowerBoundary) / 2   
+    angularSpeed = 2 * math.pi * freq
+    y = []
+
+    for time in range(int(1/freq)):
+        temp = midValue + ((upperBoundary - lowerBoundary) / 2) * math.sin(angularSpeed * time + initialDelta)
+
+        y += [temp]
+
+    return y
+
+roomId = 'room1'
+timeLength = 100
+
+# Creating Plotting Lists
+x = []
+y = []
+
+# Temperature Limits and Average
+lowerLimit = 22
+upperLimit = 29
+avgTemp = lowerLimit + (upperLimit - lowerLimit) / 2
+
+
+# Initial Time
+time = 0
+
+# Generating the function
+while True:
+
+    for staticValues in range(10):
+        y += [avgTemp]
+
+    randValue = random.randint(0,1)
+    
+    if randValue == 0:
+        ysin = sin(avgTemp, lowerLimit, 1/16, math.pi / 2)
+    else:
+        ysin = sin(upperLimit, avgTemp, 1/28, math.pi * (-1) / 2)
+
+    y += ysin
+    time += 1
+    staticValues = 0
+
+    if len(y) > timeLength:
+        break
+
+for i in range(len(y)):
+    x += [i]
+
+fileObject = open('/home/cablan/Desktop/thesisFiles/inputs/room1Sin.txt', 'w')
+for eachTemp in y:
+    fileObject.write(roomId + ',' + str(eachTemp) + '\n')
+
+# Creating the plot  
+# plotting the points  
+plt.plot(x, y) 
+  
+# naming the x axis 
+plt.xlabel('Time (s)') 
+# naming the y axis 
+plt.ylabel('Temperature (Celsius)') 
+  
+# giving a title to my graph 
+plt.title('Chamber 1') 
+  
+# function to show the plot 
+plt.show()
